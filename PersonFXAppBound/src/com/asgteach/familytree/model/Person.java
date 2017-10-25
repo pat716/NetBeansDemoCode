88d6978f46617aca95d31788d478cfeb9e30dc2f
Unknown <psweeney@applieddefense.com>
2017-10-25 14:32:00 -0400

    Saving changes to Person

--- PersonFXAppBound/src/com/asgteach/familytree/model/Person.java
+++ PersonFXAppBound/src/com/asgteach/familytree/model/Person.java
@@ -6,7 +6,11 @@
 package com.asgteach.familytree.model;
 
 import java.io.Serializable;
+import javafx.beans.binding.StringBinding;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -24,5 +28,125 @@ public class Person implements Serializable{
     private final StringProperty lastname = new SimpleStringProperty(this, "lastname", "");
     private final StringProperty suffix = new SimpleStringProperty(this, "suffix", "");
     
-    private final ObjectProperty<
+    private final ObjectProperty<Gender> gender = 
+            new SimpleObjectProperty<>(this, "gender", Gender.UNKNOWN);
+    private final StringProperty notes = new SimpleStringProperty(this, "notes", "");
+    
+    private final StringBinding fullNameBinding = new StringBinding() {
+        {
+            super.bind(firstname, middlename, lastname, suffix);
+        }
+        
+        @Override
+        protected String computeValue() {
+            StringBuilder sb = new StringBuilder();
+            if(!firstname.get().isEmpty()){
+                sb.append(firstname.get());
+            }
+            if(!middlename.get().isEmpty()){
+                sb.append(middlename.get());
+            }
+            if(!lastname.get().isEmpty()){
+                sb.append(lastname.get());
+            }
+            if(!suffix.get().isEmpty()){
+                sb.append(suffix.get());
+            }
+            return sb.toString();
+        }
+    };
+    
+    private final ReadOnlyStringWrapper fullname = new ReadOnlyStringWrapper(this, "fullname");
+    private static long count = 0;
+    
+    public Person(){
+        this("", "", Gender.UNKNOWN);
+    }
+    
+    public Person(String first, String last, Gender gender){
+        this.firstname.set(first);
+        this.lastname.set(last);
+        this.gender.set(gender);
+        this.id = count++;
+    }
+    
+    public Person(Person person){
+        
+    }
+    
+    public final long getId(){
+        return id;
+    }
+    
+    public final ReadOnlyStringProperty fullnameProperty(){
+        return fullname.getReadOnlyProperty();
+    }
+    
+    public final String getFullname(){
+        return fullname.get();
+    }
+    
+    public String getNotes(){
+        return notes.get();
+    }
+    
+    public void setNotes(String notes){
+        this.notes.set(notes);
+    }
+    
+    public final StringProperty notesProperty(){
+        return notes;
+    }
+    
+    public String getFirstname(){
+        return firstname.get();
+    }
+    
+    public void setFirstname(String firstname){
+        this.firstname.set(firstname);
+    }
+    
+    public final StringProperty firstnameProperty(){
+        return firstname;
+    }
+    
+    
+    public String getMiddlename(){
+        return middlename.get();
+    }
+    
+    public void setMiddlename(String middlename){
+        this.middlename.set(middlename);
+    }
+    
+    public final StringProperty middlenameProperty(){
+        return middlename;
+    }
+    
+    
+    public String getLastname(){
+        return lastname.get();
+    }
+    
+    public void setLastname(String lastname){
+        this.lastname.set(lastname);
+    }
+    
+    public final StringProperty lastnameProperty(){
+        return lastname;
+    }
+    
+    public String getSuffix(){
+        return suffix.get();
+    }
+    
+    public void setSuffix(String suffix){
+        this.suffix.set(suffix);
+    }
+    
+    public final StringProperty suffixProperty(){
+        return suffix;
+    }
 }
+
+
